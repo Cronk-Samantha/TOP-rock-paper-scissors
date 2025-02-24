5101f3bff8ec986eacd09566c5ef590adb6450fe
Cronk-Samantha <195992994+Cronk-Samantha@users.noreply.github.com>
2025-02-24 14:42:24 -0700

    Created the playRound function.
    The playRound function takes 2 arguments, the humanChoice
    and computerChoice, evaluates each value & declares
    a winner based on the value of each variable. It then
    adds 1 to either the computer's score or the user's
    score.

--- java.js
+++ java.js
@@ -10,13 +10,36 @@ function getComputerChoice() {
 }
 
 let getHumanChoice = () => {
-    let humanChoice = prompt("rock, paper, or scissors?: ").toLowerCase();
-    if(humanChoice != "rock" || humanChoice != "paper" || humanChoice != "scissors") {
-        humanChoice = prompt("Not a valid choice, please enter again: ").toLowerCase;
-    } else {
-        return humanChoice;
+    let humanChoice;
+    while(true) {
+        humanChoice = prompt("rock, paper, or scissors?: ").toLowerCase();
+        if(humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors") {
+            return humanChoice;
+        }
+        alert("Not a valid option, please try again.");
     }
 }
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if(humanChoice === computerChoice) {
+        console.log(`You both chose ${humanChoice}, its a tie!`);
+    } else if(
+        (humanChoice == "rock" && computerChoice == "scissors") ||
+        (humanChoice == "paper" && computerChoice == "rock") ||
+        (humanChoice == "scissors" && computerChoice == "paper") 
+    ) {
+        humanScore++;        
+        console.log(`${humanChoice} beats ${computerChoice}, you win!!`);
+    } else {
+        computerScore++;
+        console.log(`${computerChoice} beats ${humanChoice}, you lose :(`);
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
